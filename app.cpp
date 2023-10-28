17dd3880ec4e3c66ade4ac3117417ddff910fcf4
unknown <omar.ashraf.zeinhom@Hotmail.com>
2023-10-29 01:33:28 +0200

    ADDED Variables Advanced Knoweldge Video 11

--- app.cpp
+++ app.cpp
@@ -1,54 +1,74 @@
 #include <iostream>
 using namespace std;
 
-/* ANDGOEDU */
+/* <***| ANDGOEDU |***> */
 
-// Comments Example
+// Singe Line Comment Example
 
 /* Multiple Line Comments Example */
-
 
 int main()
 {
     std::cout << "I am a Dragon";
     std::cout /* <= Comment here => */ << "2nd Line \n";
     std::cout << "3rd Line \n";
-    
-/*
-Variables Basic Knowledge 
 
-- Data Container with Unique Name "Identifier"
-- Declare with Value + Change Later
-    Syntax 
-    - [Data_Type] [Variable_Name] = [Value]
+    /*
+    Variables Basic Knowledge
 
-    - Naming Rules 
-     - Unique 
-     - Case Sensitive
-     - Cannot Start with numbers
-     - Numbers or letters or underscore
-     - No White Space or Special Characters
-     - Reserved Keyword: "Class", "Public"
-     
-    - Best Practices
-        - Related Names
-        - Writing Style
-*/
+    - Data Container with Unique Name "Identifier"
+    - Declare with Value + Change Later
+        Syntax
+        - [Data_Type] [Variable_Name] = [Value]
+
+        - Naming Rules
+         - Unique
+         - Case Sensitive
+         - Cannot Start with numbers
+         - Numbers or letters or underscore
+         - No White Space or Special Characters
+         - Reserved Keyword: "Class", "Public"
+
+        - Best Practices
+            - Related Names
+            - Writing Style
+    */
     int price = 100; // Declaring type of Variable price
     std::cout << "\n Price is :" << price;
     std::cout << "\n Price After Adding 15 Is " << 100 + 15;
     std::cout << "\n Price After Adding 50 Is " << 100 + 50;
-    price = 150;    // Updating var Value
+    price = 150; // Updating var Value
     std::cout << "\n New Price is " << price;
     /* Variables Examples*/
+
     // int 1num = 1; // Cannot Start with a number
     int num1 = 1;
     int num2_ = 2;
     int num_3_ = 3;
     int _numbers = 100; // Can start var with an underscore
 
+    /*
+        Variables Advanced Knoweldge
+            - Declare Variable without value + change letter
+            - Declare multiple Variables without values + change letter
+            - Declare multiple variables with same value
+    */
+
+    int a = 100;
+
+    cout << "\n===========\n";
+
+    int a, b, c;            // Declaring multiple variables with int type
+    a = 10, c = 20, c = 30; // Updating Multiple Variables
+    cout << a + b + c;      // 10 + 20 + 30 = 60   - Printing Values with + operator
+
+    int e, f = 20, g; // Declare variables and value
+    e = 20, g = 10;   // updating values for variables without a value
+    cout << e + f + g;
+
+    int h, g, k;
+    h = g = k = 10; // All variables have the same value
+    cout << h + g + k;
 
     return 0;
 }
-
-
